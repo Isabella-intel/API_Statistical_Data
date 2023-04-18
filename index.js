0b79d96cdf165ef0e011bdccc8a6176d6b4adce7
Isabella Wagner <Bbug4102@gmail.com>
2023-04-18 05:54:53 +0000

    Code structure updated, app NOT running.

--- index.js
+++ index.js
@@ -1,52 +1,131 @@
-const express = require('express')        //Is the server on which the API runs.
-const app = express()                     //Calls the package to create an app variable.
 const cheerio = require('cheerio')        //Scrapes the html data off of webpages.
 const axios = require('axios').default    //Makes the http requests.
+const express = require('express')        //Is the server on which the API runs.
+const app = express()                     //Calls the package to create an app variable.
+const PORT = process.env.PORT || 7000
 
 
-const PORT = 7000
-app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
-
-const articles = []
-const newspapers = [ 
+const newspapers = [        //listing of websites to be scrubbed.
+    {
+        name: 'cityam',
+        address: 'https://www.cityam.com/london-must-become-a-world-leader-on-climate-change-action/',
+        base: ''
+    },
     {
         name: 'thetimes',
         address: 'https://www.thetimes.co.uk/environment/climate-change',
+        base: ''
     },
     {
-        name: 'gardian',
-        // address: 'https://www.theguardian.com/environment/climate-crisis'
+        name: 'guardian',
+        address: 'https://www.theguardian.com/environment/climate-crisis',
+        base: '',
     },
     {
-        name: 'telegram',
-        address: ''
+        name: 'telegraph',
+        address: 'https://www.telegraph.co.uk/climate-change',
+        base: 'https://www.telegraph.co.uk',
+    },
+    {
+        name: 'nyt',
+        address: 'https://www.nytimes.com/international/section/climate',
+        base: '',
+    },
+    {
+        name: 'latimes',
+        address: 'https://www.latimes.com/environment',
+        base: '',
+    },
+    {
+        name: 'smh',
+        address: 'https://www.smh.com.au/environment/climate-change',
+        base: 'https://www.smh.com.au',
+    },
+    {
+        name: 'un',
+        address: 'https://www.un.org/climatechange',
+        base: '',
+    },
+    {
+        name: 'bbc',
+        address: 'https://www.bbc.co.uk/news/science_and_environment',
+        base: 'https://www.bbc.co.uk',
+    },
+    {
+        name: 'es',
+        address: 'https://www.standard.co.uk/topic/climate-change',
+        base: 'https://www.standard.co.uk'
+    },
+    {
+        name: 'sun',
+        address: 'https://www.thesun.co.uk/topic/climate-change-environment/',
+        base: ''
+    },
+    {
+        name: 'dm',
+        address: 'https://www.dailymail.co.uk/news/climate_change_global_warming/index.html',
+        base: ''
+    },
+    {
+        name: 'nyp',
+        address: 'https://nypost.com/tag/climate-change/',
+        base: ''
     }
 ]
+const articles = []
 
-newspapers.forEach(element => {
-    axios.get(element.address)
+newspapers.forEach(newspaper => { //Callback function, being called on each element from website list.
+    axios.get(newspaper.address)
         .then(response => {
             const html = response.data
             const $ = cheerio.load(html)
-            $('a:contains("climate")', html).each(function (){
-                const title = $(this).text
+
+            $('a:contains("climate")', html).each(function () {
+                const title = $(this).text()
                 const url = $(this).attr('href')
 
                 articles.push({
                     title,
-                    url,
-                    source: newspapers.name
+                    url: newspaper.base + url,
+                    source: newspaper.name
                 })
             })
+
         })
 })
 
+app.get('/', (req, res) => {
+    res.json('Welcome to my Climate Change News API')
+})
 
-app.get('/homepage', (req, res) => { //Creating the first end point "/homepage".
-    res.json("Welcome brethren")
-}) 
-
-
-app.get('/news',(req, res) => { 
+app.get('/news', (req, res) => {
     res.json(articles)
 })
+
+app.get('/news/:newspaperId', (req, res) => {
+    const newspaperId = req.params.newspaperId
+
+    const newspaperAddress = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].address
+    const newspaperBase = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].base
+
+
+    axios.get(newspaperAddress)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+            const specificArticles = []
+
+            $('a:contains("climate")', html).each(function () {
+                const title = $(this).text()
+                const url = $(this).attr('href')
+                specificArticles.push({
+                    title,
+                    url: newspaperBase + url,
+                    source: newspaperId
+                })
+            })
+            res.json(specificArticles)
+        }).catch(err => console.log(err))
+})
+
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
